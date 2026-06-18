0c07bcdbd16032e55841ab42adbb926d3e57316f
Maurya Suryakant <mauryasuryakant99@gmail.com>
2026-06-18 20:51:14 +0530

    fix: reorder message display for improved readability

--- websocket-chat/frontend/src/App.jsx
+++ websocket-chat/frontend/src/App.jsx
@@ -185,14 +185,14 @@ export default function App() {
                                                 ? 'bg-[#DCF8C6] text-[#303030] rounded-br-2xl'
                                                 : 'bg-white text-[#303030] rounded-bl-2xl'
                                         }`}>
-                                        <div className="break-words whitespace-pre-wrap">
-                                            {m.text}
-                                        </div>
                                         <div className="flex justify-between items-center mt-1 gap-16">
                                             <div className="text-[11px] font-bold">{m.sender}</div>
                                             <div className="text-[11px] text-gray-500 text-right">
                                                 {formatTime(m.ts)}
                                             </div>
+                                        </div>
+                                        <div className="break-words whitespace-pre-wrap">
+                                            {m.text}
                                         </div>
                                     </div>
                                 </div>
