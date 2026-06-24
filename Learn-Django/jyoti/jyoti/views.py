4dc1aa2f783cbd6a6b68c774374724cc93d373ae
Jyoti Giri <jyotigiri7219@gamil.com>
2026-06-24 19:02:43 +0530

    Co-authored-by: Suryakant Maurya <mauryasuryakant@users.noreply.github.com>

--- Learn-Django/jyoti/jyoti/views.py
+++ Learn-Django/jyoti/jyoti/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("HELLO , WORLD. you are at shop home cycle")
+    
+    return render(request,'intex.html')
+ #HttpResponse("HELLO , WORLD. you are at shop home cycle")
 
 def about(request):
     return HttpResponse("HELLO , WORLD. you are at shop cycle")
