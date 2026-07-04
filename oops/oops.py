b9b4c56c308fb6bab8cc739883e5ea26da88784c
Jyoti Giri <jyotigiri7219@gamil.com>
2026-07-04 15:41:33 +0530

    feat: add initial implementation of Abhi class with attribute and method

---
+++ oops/oops.py
@@ -0,0 +1,11 @@
+class Abhi:
+    a = 10 # attribute
+
+    def hello(self): # method 
+        print("Hello world")
+
+    print("this is class abhi")
+
+print(Abhi().a) # accessing attribute
+
+Abhi().hello() # accessing method
